b5616c50a21b14bf576e05f561b3307087bbb22e
HangBeni <hangbenjamin2003@gmail.com>
2023-02-01 16:08:27 +0100

    #

--- src/main/java/com/forlearn/LoginWindowController.java
+++ src/main/java/com/forlearn/LoginWindowController.java
@@ -10,7 +10,7 @@ import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 
-public class PrimaryController implements Initializable {
+public class LoginWindowController implements Initializable {
 
     @FXML
     PasswordField userPassword;
