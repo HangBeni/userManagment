8c2b6ca6744053de2f1877ae80e5a8deb1a1bb34
HangBeni <hangbenjamin2003@gmail.com>
2023-02-01 16:12:24 +0100

    Chekkers

--- src/main/java/com/forlearn/LoginWindowController.java
+++ src/main/java/com/forlearn/LoginWindowController.java
@@ -6,9 +6,11 @@ import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.scene.control.Alert.AlertType;
 
 public class LoginWindowController implements Initializable {
 
@@ -22,9 +24,40 @@ public class LoginWindowController implements Initializable {
     Label passwordLabel;
     @FXML
     private void handleLogin(ActionEvent event){
-        nameLabel.setText("Nem jó a név");
-        passwordLabel.setText("Nem jó a jelszó");
+        nameCheck(nameLabel.getText());
+        passwordCheck(userPassword.getText());
+    }
 
+    private void passwordCheck(String text) {
+        int textLength = text.length();
+        Alert alert = new Alert(AlertType.INFORMATION);
+        alert.setTitle("Check");
+        alert.setContentText("A jelszó mező hossza: "+textLength);
+        if( textLength < 8 || textLength == 0 || textLength > 15 ){
+            userName.setStyle("-fx-border-color: red");
+            if (textLength < 8) {
+                passwordLabel.setText("Túl kevés karakter, 8 karakternél több kell, még "+textLength+" db kell");
+            } else if(textLength == 0) {
+                passwordLabel.setText("Nem írtál be semmit haló");
+            }else{
+                passwordLabel.setText("Túl sok karakter tesvérem");
+            }
+            }
+    }
+
+    private void nameCheck(String text) {
+        Alert alert = new Alert(AlertType.INFORMATION);
+        alert.setTitle("Check");
+        int names = text.split(" ").length;
+        alert.setContentText("A név mezőben: "+names+ " név van");
+        if(names <2  || names ==0){
+            nameLabel.setText("Nem jó a név mező, legalább egy kereszt és vezetéknév kell");
+            userName.setStyle("-fx-border-color: red");
+        }
+        else if(names > 4){
+        nameLabel.setText("Nem jó a név mező, túl sok név");
+        userName.setStyle("-fx-border-color: red");
+        }
     }
 
     @Override
